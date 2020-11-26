c101e87836353f057fc26b17ccf205068cb89f95
Cody Yuill <codyyuill@hotmail.com>
2020-11-26 15:12:49 -0500

    created submit initials input that appears when timer runs out and dissapears when start button is clicked

--- script.js
+++ script.js
@@ -2,30 +2,47 @@ const timerFinishEvent = new Event('timerDone');
 
 var time = 75;
 var timePTag = document.getElementById("Timer");
+
 var startBtn = document.getElementById("startBtn");
+
 var hsList = document.getElementById("highscoresList");
-var initialInput = document.getElementById("initialInput");
+
+var submitHighscoreDiv = document.getElementById("submitHighscore");
+var initalsInputEl = document.getElementById("initalsInput");
+var submitInitalsBtn = document.getElementById("submitBtn");
 
 function startTimer(){
+    startBtn.disabled = true;
     var timer = setInterval(function(){
         time--;
         timePTag.textContent = `${time} seconds remaining`;
-        console.log(time);
+        //console.log(time);
         if(time == 0)
         {
             clearInterval(timer);
-            //TODO call custom event
-            initialInput.dispatchEvent(timerFinishEvent);
+            submitHighscoreDiv.dispatchEvent(timerFinishEvent);
         }
-    }, 1000);
+    }, 1);
 }
 
 function getInput(){
-    initialInput.setAttribute("style", "display: block;");
+    submitHighscoreDiv.setAttribute("style", "display: block;");
+}
+function submitScore(){
+    var listItem = document.createElement('li');
+    listItem.textContent = initalsInputEl.value;
+    alert(listItem.textContent);
+    hsList.append(listItem);
+    submitHighscoreDiv.setAttribute("style", "display: none;");
+    time = 75;
+    timePTag.textContent = `${time} seconds remaining`;
+    startBtn.disabled = false;
+    //TODO also submit whatever score the user gets
 }
 
 
 
 timePTag.textContent = `${time} seconds remaining`;
 startBtn.addEventListener("click", startTimer);
-initialInput.addEventListener("timerDone", getInput);
+submitInitalsBtn.addEventListener("click", submitScore);
+submitHighscoreDiv.addEventListener("timerDone", getInput);
